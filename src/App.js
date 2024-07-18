90566cb18049657b013602a363176db839f57513
nishi <nishipriyad@gmail.com>
2024-07-18 16:14:51 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+// write your code here
+const API_KEY = '48b64432090d1a026db52422156808d0'
+
+const App = () => {
+    
+}
+
+export default App
---
+++ src/constants/navbar.jsx
@@ -0,0 +1,8 @@
+import {IoHome} from 'react-icons/io5'
+
+export const navbarData = [
+  {
+    label: 'Home',
+    icon: <IoHome />,
+  },
+]
